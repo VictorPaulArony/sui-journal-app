cad09d26c829f08714057fb3e90ba487e18ecdeb
victorpaularony <smallypauls@gmail.com>
2025-10-16 21:44:28 +0300

    feat: Configure network settings for journal and GraphQL

--- src/networkConfig.ts
+++ src/networkConfig.ts
@@ -1,8 +1,8 @@
 import { getFullnodeUrl } from "@mysten/sui/client";
 import {
   DEVNET_COUNTER_PACKAGE_ID,
-  MAINNET_COUNTER_PACKAGE_ID,
   TESTNET_JOURNAL_PACKAGE_ID,
+  MAINNET_COUNTER_PACKAGE_ID,
 } from "./constants.ts";
 import { createNetworkConfig } from "@mysten/dapp-kit";
 
@@ -14,19 +14,20 @@ const { networkConfig, useNetworkVariable, useNetworkVariables } =
         counterPackageId: DEVNET_COUNTER_PACKAGE_ID,
       },
     },
-     testnet: {
-  url: getFullnodeUrl("testnet"),
-  variables: {
-    journalPackageId: TESTNET_JOURNAL_PACKAGE_ID,
-  },
+    testnet: {
+      url: getFullnodeUrl("testnet"),
+      variables: {
+        journalPackageId: TESTNET_JOURNAL_PACKAGE_ID,
+        graphqlUrl: "https://graphql.testnet.sui.io/graphql",
+      },
+    },
+
     mainnet: {
       url: getFullnodeUrl("mainnet"),
       variables: {
         counterPackageId: MAINNET_COUNTER_PACKAGE_ID,
       },
     },
-   
-},
   });
 
 export { useNetworkVariable, useNetworkVariables, networkConfig };
